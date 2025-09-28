6413487b9479c02807f6532db33f2e5fca327d8b
nnsang824 <sangchuadao123@gmail.com>
2025-09-28 10:15:29 +0700

    Update App.js with greeting message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Xin chào! 🚀</h1>
+      <p>Đây là website React đầu tiên của tôi</p>
+      <p>Đã deploy thành công lên Cloudflare Pages 🎉</p>
     </div>
   );
 }
